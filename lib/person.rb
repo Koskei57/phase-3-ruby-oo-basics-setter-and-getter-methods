c19e46314407722a3594e506393f11d1aab6bdb8
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-08-18 00:20:53 +0300

    commit tree 43be080985c3f52512410fd11fcbe85782b6d477

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
-
+  def name=(name)
+   @name = name
+ end
+ def name
+  @name
+ end
+def job=(job)
+ @job = job
+ end
+ def job
+  @job
+ end
 end
+ Dennis = Person.new
+ Dennis.name = "Kanye"
+Dennis.name
